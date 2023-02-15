5cd8d39684f363ca4fffdc607fd6b6ba798eabec
Shibata-8bit <sbtbnnsk0712@gmail.com>
2023-02-15 17:05:33 +0900

    defファイルの定数を追加

---
+++ db/def.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  DB接続情報
+*/
+// 管理者アカウント
+define("DB_HOST", "localhost");
+define("DB_USER", "Administrator");
+define("DB_PASS", "Administratorasagod");
+define("DB_NAME", "try_hack_us");
+define("DB_CHARSET", "utf8mb4");
+
+// ダミーアカウント
+// define("DB_HOST", "localhost");
+// define("DB_USER", "dummyUser");
+// define("DB_PASS", "iamking");
+// define("DB_NAME", "try_hack_us");
+// define("DB_CHARSET", "utf8mb4");
