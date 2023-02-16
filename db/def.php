0add275d5edbeeb0ead28e8a1196dc0a40e165b6
Shibata-8bit <sbtbnnsk0712@gmail.com>
2023-02-16 14:34:14 +0900

    db接続の基礎完了

--- db/def.php
+++ db/def.php
@@ -15,3 +15,30 @@ define("DB_CHARSET", "utf8mb4");
 // define("DB_PASS", "iamking");
 // define("DB_NAME", "try_hack_us");
 // define("DB_CHARSET", "utf8mb4");
+
+class dbConnection
+{
+  function connection()
+  {
+    try {
+      // データソース名を設定
+      $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET;
+      // $dbにPDOのインスタンス生成
+      $db = new PDO($dsn, DB_USER, DB_PASS);
+
+      //　PODの動作オプションを指定
+      $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);                 // ステートメントのエミュレーションをオフ
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);         // エラーを表示する
+      $db->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);    // 空文字をnullに変換
+      $db->setAttribute(PDO::ATTR_CASE, PDO::CASE_UPPER);                   // 小文字を大文字に変換
+      $db->setAttribute(PDO::ATTR_AUTOCOMMIT, false);                       // autocommitをオフ
+
+      return $db;
+    } catch (PDOException $poe) {
+      echo $poe;
+    } finally {
+      // nullにして接続を終了
+      $db = null;
+    }
+  }
+}
--- register/sign_in.php
+++ register/sign_in.php
@@ -1,11 +1,31 @@
 <?php
+// データベースの接続情報が書かれているファイルを読み込み
+require_once "../db/def.php";
+
+// ログイン情報の受け取り
+$username = filter_input(INPUT_POST, "username");
+$password = filter_input(INPUT_POST, "password");
 
 // 送られてきた情報管理配列
 $result = [
   "status" => true,
   "errMsg" => "",
-  "Flag" => 0
 ];
+
+// usernameとpasswordが送られてきていた場合のみ
+if (isset($username) && isset($password)) {
+  try {
+    // データベースに接続
+    $dbConnection = new dbConnection();
+    $db = $dbConnection->connection();
+  } catch (PDOException $e) {
+    echo $e;
+  } finally {
+    // nullにして接続を終了
+    $db = null;
+  }
+}
+
 ?>
 <?php $title = "トップページ"; ?> <!-- headのtitleに反映させる -->
 <?php $description = "トップページの説明"; ?> <!-- headのdescriptionに反映させる -->
@@ -16,10 +36,12 @@ $result = [
 
 <div class="login-page">
   <div class="form">
-    <form class="login-form">
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
-      <button>LOGIN</button>
+    <form action="./sign_in.php" method="POST">
+      <form class="login-form">
+        <input type="text" name="username" placeholder="username" />
+        <input type="password" name="password" placeholder="password" />
+        <button>LOGIN</button>
+      </form>
     </form>
   </div>
 </div>
