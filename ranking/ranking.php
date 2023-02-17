669a775562d19bb6d30808c58f3f890ce4e1e03e
Shibata-8bit <sbtbnnsk0712@gmail.com>
2023-02-17 15:00:08 +0900

    [update] rankingページを更新

--- ranking/ranking.php
+++ ranking/ranking.php
@@ -1,18 +1,40 @@
+<?php $title = "ランキング一覧"; ?> <!-- headのtitleに反映させる -->
+<?php $description = "Let's hack"; ?> <!-- headのdescriptionに反映させる -->
+<?php include("../_inc/header.php"); ?> <!-- ヘッダー共通部分 -->
+
 <?php
 
 // データベースの接続情報ファイルを読み込み
 require_once "../db/def.php";
 
-$dbConnection = new dbConnection();
-$db = $dbConnection->connection();
+try {
+  // DB接続をインスタンス化
+  $dbConnection = new dbConnection();
+  $db = $dbConnection->connection();
 
+  // SQL文を作成
+  $sql = "SELECT username,score
+        FROM users;";
 
+  // stmtにsql文をセット
+  $stmt = $db->prepare($sql);
 
+  // 実行
+  $stmt->execute();
+
+  // 実行結果を取得
+  $result = [];
+  while ($rows = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $result[] = $rows;
+  }
+} catch (PDOException $e) {
+  echo $e;
+} finally {
+  $db = null;
+  $stmt = null;
+}
 ?>
 
-<?php $title = "ランキング一覧"; ?> <!-- headのtitleに反映させる -->
-<?php $description = "Let's hack"; ?> <!-- headのdescriptionに反映させる -->
-<?php include("../_inc/header.php"); ?> <!-- ヘッダー共通部分 -->
 <link rel="stylesheet" href="../css/ranking.css"> <!-- css読み込み -->
 <!-- ▼content▼ -->
 <div class="content">
