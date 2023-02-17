f3a351c5a4f5016c04f3657902cd5b771e7e9ce0
Shibata-8bit <sbtbnnsk0712@gmail.com>
2023-02-17 14:01:29 +0900

    headerの無駄な空白を削除

--- _inc/header.php
+++ _inc/header.php
@@ -4,7 +4,7 @@
 session_start();
 
 //ログイン状態出ない場合sign_in.phpページにリダイレクト
-if(!isset($_SESSION["login"])){
+if (!isset($_SESSION["login"])) {
    header("Location: ../register/sign_in.php");
 }
 
@@ -14,38 +14,40 @@ if(!isset($_SESSION["login"])){
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width,initial-scale=1.0">
-<title><?=$title?></title> <!-- 各ページのtitleを反映させる -->
-<meta name="description" content="<?=$description?>"> <!-- 各ページのdescriptionを反映させる -->
-<link rel="stylesheet" href="../css/style.css">
-<link rel = "stylesheet" href = "../css/header.css">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Babylonica&family=Playfair+Display:wght@500&display=swap" rel="stylesheet">
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width,initial-scale=1.0">
+   <title><?= $title ?></title> <!-- 各ページのtitleを反映させる -->
+   <meta name="description" content="<?= $description ?>"> <!-- 各ページのdescriptionを反映させる -->
+   <link rel="stylesheet" href="../css/style.css">
+   <link rel="stylesheet" href="../css/header.css">
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Babylonica&family=Playfair+Display:wght@500&display=swap" rel="stylesheet">
 
 </head>
+
 <body>
    <div class="wrapper">
       <header>
          <!-- ヘッダー部分 -->
-         <div class = "HeaderStart">
-            <div class = "HeaderMenu">
-               <h1><a href = "" title = "ホームページへ">Try Hack Us!</a></h1>
+         <div class="HeaderStart">
+            <div class="HeaderMenu">
+               <h1><a href="" title="ホームページへ">Try Hack Us!</a></h1>
                <ul>
-                  <li><a href = "" title = "クイズ">何やつ</a></li>
-                  <li><a href = "" title = "ランキング">何じゃ</a></li>
-                  <li><a href = "" title = "参考書">何用</a></li>
-                  <li><a href = "" title = "なんか">何様</a></li>
+                  <li><a href="" title="クイズ">何やつ</a></li>
+                  <li><a href="" title="ランキング">何じゃ</a></li>
+                  <li><a href="" title="参考書">何用</a></li>
+                  <li><a href="" title="なんか">何様</a></li>
                </ul>
             </div>
          </div>
-      </header> 
+      </header>
 
 
-   
-    
+
+
 
 
 
