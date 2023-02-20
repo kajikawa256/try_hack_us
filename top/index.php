3f17f501034ff93206a2896c8157acc61e19bce0
2220216K <2220216@ecc.ac.jp>
2023-02-20 16:51:04 +0900

    あと名前とランキングだけと思う

--- top/index.php
+++ top/index.php
@@ -2,7 +2,10 @@
 <?php $description = "トップページの説明";?> <!-- headのdescriptionに反映させる -->
 <?php include("../_inc/header.php"); ?>  <!-- ヘッダー共通部分 -->
 <link rel="stylesheet" href="../css/top.css">
-
+<?php   
+        //フォームからusernameを取得
+        //$name = filter_input(INPUT_GET,"username");
+ ?>
        <div class="contents">
                 <!-- コンテンツ部分 -->
                 <h2 class="tops">あなたのセキュリティは本当に安全ですか？</h2>
@@ -10,8 +13,8 @@
                 <p>ランキング</p>
                 <p>1位<br>@@@</p>
                 </div>
-                <h1 class="top">ようこそ！<br>〇〇さん</h1>
-
+                <h1 class="top">ようこそ！<br>さん</h1>
+                <!-- 問題に移動 -->
                 <button class="btn" onclick="location.href='../question/ques_page_1.php'">問題へ</button>
 
                 <div class="tabs">
@@ -20,18 +23,18 @@
                         <input id="programming" type="radio" name="tab_item">
                         <label class="tab_item" for="programming">脆弱性の例</label>
                         <input id="design" type="radio" name="tab_item">
-                        <label class="tab_item" for="design">ヴぁヴぁばぁ</label>
+                        <label class="tab_item" for="design">脆弱性対策</label>
                         <!-- 脆弱性とは -->
                         <div class="tab_content" id="all_content">
                                 <div class="tab_content_description">
-                                        <p class="c-txtsp">コンピュータのOSやソフトウェアにおいて、プログラムの不具合や設計上のミスが原因となって発生した情報セキュリティ上の欠陥のことを言います。</p>
+                                        <p class="c-txtsp">コンピュータのOSやソフトウェアにおいて、プログラムの不具合や設計上のミスが原因となって発生した情報セキュリティ上の欠陥のことを言います。 脆弱性には深刻な影響を及ぼすものもあり、悪用されるとシステムを乗っ取られたり、重要な情報が漏えいしたり、別の攻撃の踏み台にされる危険性があります</p>
                                 </div>
                         </div>
                         <!-- 例 -->
                         <div class="tab_content" id="programming_content">
                                 <div class="tab_content_description">
                                         <p class="c-txtsp">
-                                                <ul>
+                                                <ul class="topul">
                                                         <li>SQLインジェクション</li>
                                                         <li>クロスサイト・スクリプティング</li>
                                                         <li>CSRF（クロスサイト・リクエスト・フォージェリ）
@@ -49,11 +52,13 @@
                         <!--  -->
                         <div class="tab_content" id="design_content">
                                 <div class="tab_content_description">
-                                        <p class="c-txtsp">ヴぁヴぁばぁ</p>
+                                        <p class="c-txtsp">①最新の実装ガイドを確認して脆弱性となるポイントをなくす
+                                                        <br>②ソフトウェアアップデートを適用する
+                                                        <br>③定期的に診断を行う
+                                                        <br>④脆弱性対策ツールを使用して、脆弱性となるポイントを洗い出す</p>
                                 </div>
                         </div>
                 </div>
-
 
                 <p class="text">
                 特殊文字を適切に処理できていないと、スクリプトを実行できてしまったり、意図しない動作が行わ
