59d3c038f8d86b6fdd9b3454bfaff9828af89de2
2220216K <2220216@ecc.ac.jp>
2023-02-21 16:46:10 +0900

    参考書作成中

---
+++ books/index.php
@@ -0,0 +1,26 @@
+<?php $title = "トップページ";?> <!-- headのtitleに反映させる -->
+<?php $description = "トップページの説明";?> <!-- headのdescriptionに反映させる -->
+<?php include("../_inc/header.php"); ?>  <!-- ヘッダー共通部分 -->
+<link rel="stylesheet" href="../css/books.css">
+    <div class="contents">
+        <h1>参考書</h1>
+        <div id="parent">
+            <div class="img"><img src="../img/img01.png"></div>
+            <div class="imgex">
+                <p>「図解即戦力 情報セキュリティの技術と対策がこれ１冊でしっかりわかる教科書」</p>
+                情報セキュリティに関する基本的な知識を、キーワード形式で、イラストを使ってわかりやすく解説しています。出典：Amazon
+                当書では情報セキュリティの概念からサイバー攻撃の手法、対策方法など情報セキュリティに関する基本的な知識について解説されております。
+                情報セキュリティマネジメント試験の午前重点分野で出題される項目が中心となっているので資格取得に興味のある方にもおすすめです。
+                イラストが豊富に用いられており、初学者向けにわかりやすく解説されている
+            </div>
+        </div>
+        <div id="parent">
+            <div class="img"><img src="../img/img01.png"></div>
+            <div class="imgex">
+                <p>題名</p>
+                内容
+            </div>
+        </div>
+
+    </div>
+<?php include("../_inc/footer.php"); ?> <!-- フッター共通部分 -->
