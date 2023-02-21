48951d75c494940a7a76db4cf9e076e4a0079936
2220054 <2220054@ecc.ac.jp>
2023-02-21 16:49:37 +0900

    header change

---
+++ _inc/header_login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width,initial-scale=1.0">
+   <title><?= $title ?></title> <!-- 各ページのtitleを反映させる -->
+   <meta name="description" content="<?= $description ?>"> <!-- 各ページのdescriptionを反映させる -->
+   <link rel="stylesheet" href="../css/style.css">
+   <link rel="stylesheet" href="../css/header.css">
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Babylonica&family=Playfair+Display:wght@500&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+   <div class="wrapper">
+      <header>
+        <div class = "HeaderStart">
+            <div class = "top">
+                <h1>Try Hack Us!</h1>
+            </div>
+        </div>
+      </header>
+   </div>
+</body>
